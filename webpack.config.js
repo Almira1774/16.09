d6ea1e0f6c5e4fbcd28a00a033a1e406fe3db856
Almira Vasiluk <bezzbashennaya@gmail.com>
2025-09-23 08:04:07 +0300

    установила swiper

--- webpack.config.js
+++ webpack.config.js
@@ -4,58 +4,65 @@ const { watchFile } = require('fs');
 
 
 module.exports = {
-mode: 'development',
-entry: path.resolve(__dirname, 'src/index.js'),
-output: {
-path: path.resolve(__dirname, 'dist'),
-filename: '[name][contenthash].js',
-assetModuleFilename: 'assets/[name][ext]',
-clean: true
-},
-devServer: {
-port: 9000,
-compress: true,
-hot: true,
-static: {
-directory: path.join(__dirname, 'dist'),
-},
-},
-module: {
-rules: [
-{
-test: /\.scss$/,
-use: ['style-loader', 'css-loader', 'sass-loader'],
-},
-{
-  test: /\.(png|jpe?g|gif|svg)$/i,
-  type: 'asset/resource'
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true
   },
-  {
-    test: /\.svg$/,
-    type: 'asset/resource',
-    generator: {
-        filename: path.join('icons', '[name].[contenthash][ext]'),
-    }
-  },
-  
-    {
-      test: /\.(woff2?|eot|ttf|otf)$/i,
-      type: 'asset/resource'
+  devServer: {
+    port: 9000,
+    compress: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+    },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+          filename: path.join('icons', '[name].[contenthash][ext]'),
+        }
+      },
 
-  {
-    test: /\.js$/,
-    use: 'babel-loader',
-    exclude: /node_modules/,
-  }
-],
-},
-plugins: [
-new HtmlWebpackPlugin({
-    title: 'My Web Page',
-    filename: 'index.html',
-    template: './src/index.html'
-})
-]
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'template.pug'),
+      filename: 'index.html',
+    })
+  ]
 };
 
